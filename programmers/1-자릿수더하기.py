3a9f6bfbc88c789600c7f86ca03a452822b3c407
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-06-19 16:53:07 +0900

    올바른 괄호

--- programmers/1-자릿수더하기.py
+++
@@ -1,9 +0,0 @@
-def solution(n):
-    answer = 0
-    
-    n= list(map(int, str(n)))
-
-    for i in range(len(n)):
-        answer+= n[i]
-
-    return answer
---
+++ programmers/올바른괄호.py
@@ -0,0 +1,11 @@
+def solution(s):
+    stack= []
+    for e in s:
+        if e == '(':
+            stack.append(e)
+        else:
+            if stack == []:
+                return False
+            else:
+                stack.pop()
+    return stack == []
