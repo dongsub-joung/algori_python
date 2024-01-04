3831448b5f305af839f6e2e3c5b049480d241612
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-01-04 16:48:50 +0900

    C

--- algorithm/week1/2/C.py
+++ algorithm/week1/2/C.py
@@ -1,2 +1,22 @@
 # https://www.acmicpc.net/problem/9012
 
+T = int(input())
+
+for i in range(T):
+    stack= []
+    
+    brackets= input()
+    for c in brackets:
+        if c=='(':
+            stack.append(c)
+        elif c==')':
+            if stack:
+                stack.pop()
+            else:
+                print("NO")
+                break
+    else:
+        if not stack:
+            print("YES")
+        else:
+            print("NO")
