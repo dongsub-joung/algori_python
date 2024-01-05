9091c891a86d661a65ccad87221c20a56b4feb46
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-01-05 12:45:58 +0900

    3

---
+++ algorithm/week1/3/A.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/jewels-and-stones/
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        freqs= {}
+        count= 0
+
+        for c in stones:
+            if c not in freqs:
+                freqs[c]= 1
+            else:
+                freqs[c] += 1
+        
+        for c in jewels:
+            if c in freqs:
+                count+= freqs[c]
+
+        return count
