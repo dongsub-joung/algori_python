58e0217f1ab7ee05a7efaf3c03d3869c4e5a871a
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-01-05 13:58:14 +0900

    d

---
+++ algorithm/week1/3/D.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/1920
+import sys
+from collections import Counter
+
+input= sys.stdin.readline
+
+n= int(input())
+As= list(map(int, input().split()))
+
+m= int(input())
+Am= list(map(int, input().split()))
+
